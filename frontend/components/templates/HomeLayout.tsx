a6f173a9584fcef96945e2b10096c1bc31183202
onodera <onodera@h-company.co.jp>
2025-11-23 20:32:57 +0900

    階層修正

--- frontend/components/templates/HomeLayout.tsx
+++ frontend/components/templates/HomeLayout.tsx
@@ -1,6 +1,29 @@
+"use client";
+import TodoInputCard from "../molecules/TodoInputCard";
+import { useActionState } from "react";
+import createTodoAction from "@/app/action";
 import { Todo } from "@/graphql/documents/graphql";
-import TodoForm from "../organizations/TodoForm";
+import TodoDisplayCard from "../molecules/TodoDisplayCard";
 
-export default async function HomeLayout({ todos }: { todos: Todo[] }) {
-  return <TodoForm todos={todos} />;
+export default function HomeLayout({ todos }: { todos: Todo[] }) {
+  const [state, action] = useActionState(createTodoAction, todos);
+  return (
+    <section className="container mx-auto p-4">
+      <h1 className="mx-auto text-2xl font-bold mb-4 text-center">
+        Your Tasks
+      </h1>
+      <form action={action}>
+        <input type="hidden" name="action" value="create" />
+        <TodoInputCard />
+      </form>
+      {state.map(todo => (
+        <div className="my-2" key={todo.id}>
+          <form action={action}>
+            <input type="hidden" name="action" value="delete" />
+            <TodoDisplayCard todo={todo} />
+          </form>
+        </div>
+      ))}
+    </section>
+  );
 }
